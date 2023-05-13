4108d5d7d4f165d4423bbb6c2cc0c2feb118ec61
Lucas <lucas.tx.reis@gmail.com>
2023-05-13 20:57:09 -0300

    feat: add basic components & proper folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './features/canvas/canvas.component';
+import { TextAreaComponent } from './features/text-area/text-area.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent,
+    TextAreaComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
