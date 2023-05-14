7ba9efd88a7e73b269f6dc43c048499e3b246163
Lucas <lucas.tx.reis@gmail.com>
2023-05-13 21:47:06 -0300

    feat: add text-area component

--- src/app/features/text-area/text-area.component.ts
+++ src/app/features/text-area/text-area.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-text-area',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./text-area.component.css']
 })
 export class TextAreaComponent {
+
+  defaultRows: number = 32;
+  defaultCols:number = 32;
+
+  @Input() title?: string;
+  @Input() placeholderText?: string;
+  @Input() rows?: number;
+  @Input() cols?: number;
 
 }
