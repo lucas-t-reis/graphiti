af9ce7a08e7c729852620fa58edba015726467d3
Lucas <lucas.tx.reis@gmail.com>
2023-06-09 17:20:00 -0300

    feat: canvas component

--- src/app/features/canvas/canvas.component.ts
+++ src/app/features/canvas/canvas.component.ts
@@ -3,16 +3,57 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
-  styleUrls: ['./canvas.component.css']
+  styleUrls: ['./canvas.component.css'],
 })
 export class CanvasComponent {
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+  canvasContext: CanvasRenderingContext2D;
 
-  @ViewChild('canvas') canvas !: ElementRef<HTMLCanvasElement>;
+  ngAfterViewInit(): void {
+    const context = this.canvas.nativeElement.getContext('2d');
+    if (context !== null) {
+      this.canvasContext = context;
+      this.draw();
+    }
+  }
 
+  draw(): void {
+    this.drawCircle(50, 50, 25);
+	const coord = this.drawLine(0,0, 100, 100);
+	console.log(coord);
+  }
 
+  drawCircle(x: number, y: number, radius: number,
+    startAngle = 0, endAngle = Math.PI): void {
 
-  ngAfterViewInit() : void {
+    if (x == null || y == null) return;
+    if (radius && radius <= 0) return;
+
+    this.canvasContext.beginPath();
+    this.canvasContext.moveTo(x, y);
+    this.canvasContext.arc(x, y, radius, startAngle, endAngle * 2, true);
+    this.canvasContext.stroke();
 
   }
 
+  drawLine(xOrigin: number, yOrigin: number,
+	targetX: number, targetY: number): Coordinate {
+
+    this.canvasContext.beginPath();
+    this.canvasContext.moveTo(xOrigin, yOrigin);
+    this.canvasContext.lineTo(targetX, targetY);
+    this.canvasContext.stroke();
+    return new Coordinate(targetX, targetY);
+
+  }
+}
+
+class Coordinate {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
 }
